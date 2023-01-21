b01aa25c83e64f40b15de3a20f8a8b661dd1dcef
Gabriel Mathias <gablemathias@icloud.com>
2023-01-21 10:42:00 -0300

    include legislators for each zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 class EventManager
   attr_accessor :content
@@ -10,11 +11,9 @@ class EventManager
       headers: true,
       header_converters: :symbol
     )
-    content.each do |row|
-      name = row[:first_name]
-      zipcode = clean_zipcode(row[:zipcode])
-      puts "#{name} - #{zipcode}"
-    end
+    @civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+    @civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+    show
   end
 
   private
@@ -22,4 +21,28 @@ class EventManager
   def clean_zipcode(zipcode)
     zipcode.to_s.rjust(5, "0")[0...5]
   end
+
+  def show
+    content.each do |row|
+      name = row[:first_name]
+      zipcode = clean_zipcode(row[:zipcode])
+
+      puts "#{name} - #{zipcode} - #{legislators(zipcode)}"
+    end
+  end
+
+  def legislators(zipcode)
+    legislators = @civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators.officials.map(&:name).join(', ')
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 end
+
+# Set the API Key
+# Send the query with the given criteria
+# Parse the response for the names of your legislators.
